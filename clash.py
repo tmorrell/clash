4e1d1c615194d464723291e0c0d48ed9b965d9f1
Tom Morrell <tmorrell@users.noreply.github.com>
2018-08-26 17:34:18 -0700

    Basic testing

--- clash.py
+++ clash.py
@@ -5,17 +5,17 @@ def clash(pdb,resids):
     searchstr = ''
     for r in resids:
         searchstr = searchstr + ' resid ' + str(r) + ' or '
-        searchstr = searchstr[0:-3] #remove extra or
+    searchstr = searchstr[0:-3] #remove extra or
     newp = p3d.protein.Protein(pdb)
 
-    fu = newp.query(searchstr)
-    clash = len(newp.query('within 1.0 of ',fu,' and not ',fu))
-    for j in newp.query('within 1.0 of ',fu,' and not ',fu):
-        print(j.resid,j.atype)
-    if clash < 1:
-        return False
-    else:
+    res = newp.query(searchstr)
+    clash = len(newp.query('within 1.0 of ',res,' and not ',res))
+    #for j in newp.query('within 1.0 of ',fu,' and not ',fu):
+    #    print(j.resid,j.atype)
+    if clash > 0:
         return True
+    else:
+        return False
 
 if __name__ == "__main__":
     pdbs = glob.glob('pdbs/*')
